d290deed4d666d356b64637c3ab5b917838f1872
Renzo Perez <renzin@gmail.com>
2022-01-04 17:20:38 -0500

    task files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import logo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* Header */}
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> .
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+          School dashboard
+        </h1>
+      </div>
+      {/* Body */}
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email"/>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password"/>
+        <button>OK</button>
+      </div>
+      {/* Footer */}
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
     </div>
   );
 }
